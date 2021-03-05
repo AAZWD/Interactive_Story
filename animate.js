8aa46c1c760f7eec145e2fc60035b840facfb14f
AAZWD <azumrawi@gmail.com>
2021-03-04 19:52:36 -0800

    first commit

---
+++ animate.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    
+    //Open close login modal
+    $('.begin').click(function(){
+        $('.modal').show();
+    });
+    $('.closeModal').click(function(){
+        $('.modal').hide();
+    });
+});
+
+ 
